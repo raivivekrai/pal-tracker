c98a2640d6951e3dab236c1dff4a7bcde5778d7b
Vivek Rai <mailvivekrai@gmail.com>
2018-07-27 14:25:52 -0400

    cloudNativeDeveloperDatabaseMigrations

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -3,20 +3,21 @@ set -e
 
 
 app_guid=`cf app $1 --guid`
-credentials=`cf curl /v2/apps/$app_guid/env | jq '.system_env_json.VCAP_SERVICES' | jq '.["p-mysql"][0].credentials'`
+credentials=`cf curl /v2/apps/$app_guid/env | jq '.system_env_json.VCAP_SERVICES' | jq '.["cleardb"][0].credentials'`
 
 ip_address=`echo $credentials | jq -r '.hostname'`
+db_port=`echo $credentials | jq -r '.port'`
 db_name=`echo $credentials | jq -r '.name'`
 db_username=`echo $credentials | jq -r '.username'`
 db_password=`echo $credentials | jq -r '.password'`
 
-echo "Opening ssh tunnel to $ip_address"
-cf ssh -N -L 63306:$ip_address:3306 pal-tracker &
-cf_ssh_pid=$!
+#echo "Opening ssh tunnel to $ip_address"
+#cf ssh -N -L 63306:$ip_address:3306 pal-tracker &
+#cf_ssh_pid=$!
 
-echo "Waiting for tunnel"
-sleep 5
+#echo "Waiting for tunnel"
+#sleep 5
 
-flyway-*/flyway -url="jdbc:mysql://127.0.0.1:63306/$db_name" -locations=filesystem:$2/databases/tracker -user=$db_username -password=$db_password migrate
+flyway -url="jdbc:mysql://$ip_address:$db_port/$db_name" -locations=filesystem:databases/tracker -user=$db_username -password=$db_password migrate
 
-kill -STOP $cf_ssh_pid
+#kill -STOP $cf_ssh_pid
